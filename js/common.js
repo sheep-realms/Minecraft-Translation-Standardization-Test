6fb3ba45b86e9a3d3c96cdc70ad588a105b659f0
sheep-realms <sheep_realms@qq.com>
2022-09-08 23:41:55 +0800

    更改署名方式

--- js/common.js
+++ js/common.js
@@ -13,7 +13,7 @@ function page_footer() {
                     </div>
                 </div>
                 <div class="footer-right">
-                    <div>© Sheep-realms<div>
+                    <div><a href="https://github.com/sheep-realms">Sheep-realms</a><div>
                     <div>
                         <a href="https://github.com/sheep-realms/Minecraft-Translation-Standardization-Test">Github</a> | 
                         <a href="https://minecraft.fandom.com/zh/wiki/Minecraft_Wiki:译名标准化">译名标准化</a>
